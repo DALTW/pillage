6f45dd15fd8473c4cd8b0bba36567c3f3be75908
yubin <munu220@gmail.com>
2026-06-05 21:39:11 +0900

    feat(scene): configure NewScene player and camera follow

--- Assets/camera/CameraFollow2D.cs
+++ Assets/camera/CameraFollow2D.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow2D : MonoBehaviour
@@ -5,6 +6,10 @@ public class CameraFollow2D : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f);
     [SerializeField] private float smoothSpeed = 8f;
+    [SerializeField] private float followDelay;
+    [SerializeField, Range(0f, 1f)] private float followFractionPerTick;
+
+    private readonly List<TargetSample> targetHistory = new List<TargetSample>();
 
     private void LateUpdate()
     {
@@ -13,8 +18,19 @@ public class CameraFollow2D : MonoBehaviour
             return;
         }
 
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+        targetHistory.Add(new TargetSample(Time.time, target.position));
+
+        if (!TryGetFollowTarget(out Vector3 delayedTargetPosition))
+        {
+            return;
+        }
+
+        Vector3 targetPosition = delayedTargetPosition + offset;
+        float followAmount = followFractionPerTick > 0f
+            ? followFractionPerTick
+            : smoothSpeed * Time.deltaTime;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followAmount);
     }
 
     public void SnapToTarget()
@@ -25,15 +41,58 @@ public class CameraFollow2D : MonoBehaviour
         }
 
         transform.position = target.position + offset;
+        ResetTargetHistory();
     }
 
     public void SetTarget(Transform newTarget, bool snapToTarget)
     {
         target = newTarget;
+        ResetTargetHistory();
 
         if (snapToTarget)
         {
             SnapToTarget();
+        }
+    }
+
+    private bool TryGetFollowTarget(out Vector3 followTarget)
+    {
+        if (followDelay <= 0f)
+        {
+            followTarget = target.position;
+            return true;
+        }
+
+        float targetTime = Time.time - followDelay;
+        if (targetHistory.Count == 0 || targetHistory[0].time > targetTime)
+        {
+            followTarget = default;
+            return false;
+        }
+
+        while (targetHistory.Count > 1 && targetHistory[1].time <= targetTime)
+        {
+            targetHistory.RemoveAt(0);
+        }
+
+        followTarget = targetHistory[0].position;
+        return true;
+    }
+
+    private void ResetTargetHistory()
+    {
+        targetHistory.Clear();
+    }
+
+    private readonly struct TargetSample
+    {
+        public readonly float time;
+        public readonly Vector3 position;
+
+        public TargetSample(float time, Vector3 position)
+        {
+            this.time = time;
+            this.position = position;
         }
     }
 }
